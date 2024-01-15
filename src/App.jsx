9630404b000e91d7ec5de28dc7d0a3b1bca63d70
HollowZll <118689735+HollowZll@users.noreply.github.com>
2024-01-15 13:08:59 +0100

    add: hero section and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import Test from "./Test";
+import "./app.scss";
+import HeroSection from "./components/herosection/Hero";
+import { Navbar } from "./components/navbar/navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>
+      <Navbar />
+      <HeroSection/>
+    </section>
+    <section><a href="">Hello</a></section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Paralax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Contact</section>
+    {/* <Test></Test> */}
+  </div>
 };
 
 export default App;
